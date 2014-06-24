6ff55af64a5fe2fa0191142d5e1cf48c0573e342
Logan Zhou <xorcererzc@gmail.com>
2014-06-24 13:56:23 +0800

    First commit for gridding demo. #3.

---
+++ Assets/GroundManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundManager : MonoBehaviour
+{
+    public GameObject cellPrefab;
+    public int CellWidth = 10;
+    public int CellHeight = 10;
+    public static int xCellCount = 5;
+    public static int yCellCount = 5;
+    private GameObject[,] cells = new GameObject[xCellCount, yCellCount];
+
+	void Start ()
+	{
+		for (int i = -xCellCount / 2; i < xCellCount / 2 + 1; i++)
+		{
+			for (int j = -yCellCount / 2; j < yCellCount / 2 + 1; j++)
+			{
+                Vector3 pos = new Vector3(i * CellWidth, j * CellHeight, 0);
+                var cell = (GameObject)Instantiate(cellPrefab, pos, cellPrefab.transform.rotation);
+                cells[i + yCellCount / 2, j + xCellCount / 2] = cell;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
