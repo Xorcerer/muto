bd34ca9797bcb5a25ca11a945e97712fd69b7fde
Logan Zhou <xorcererzc@gmail.com>
2014-06-24 14:09:31 +0800

    Fixed naming. #3.

--- Assets/GroundManager.cs
+++ Assets/GroundManager.cs
@@ -3,22 +3,23 @@ using System.Collections;
 
 public class GroundManager : MonoBehaviour
 {
-    public GameObject cellPrefab;
+    public GameObject CellPrefab;
     public int CellWidth = 10;
     public int CellHeight = 10;
-    public static int xCellCount = 5;
-    public static int yCellCount = 5;
-    private GameObject[,] cells = new GameObject[xCellCount, yCellCount];
+    public static int XCellCount = 5;
+    public static int YCellCount = 5;
+
+    private GameObject[,] cells = new GameObject[XCellCount, YCellCount];
 
 	void Start ()
 	{
-		for (int i = -xCellCount / 2; i < xCellCount / 2 + 1; i++)
+		for (int i = -XCellCount / 2; i < XCellCount / 2 + 1; i++)
 		{
-			for (int j = -yCellCount / 2; j < yCellCount / 2 + 1; j++)
+			for (int j = -YCellCount / 2; j < YCellCount / 2 + 1; j++)
 			{
                 Vector3 pos = new Vector3(i * CellWidth, j * CellHeight, 0);
-                var cell = (GameObject)Instantiate(cellPrefab, pos, cellPrefab.transform.rotation);
-                cells[i + yCellCount / 2, j + xCellCount / 2] = cell;
+                var cell = (GameObject)Instantiate(CellPrefab, pos, CellPrefab.transform.rotation);
+                cells[i + YCellCount / 2, j + XCellCount / 2] = cell;
 			}
 		}
 	}
