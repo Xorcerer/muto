5e3733212f320702a39029a360c4bfaa155b4eac
Logan Zhou <xorcererzc@gmail.com>
2014-11-21 08:44:12 +0800

    Limited grid world with ELM. #3.

--- elm/grid.elm
+++ elm/grid.elm
@@ -1,33 +1,50 @@
 import Keyboard
-
+import Window
 
 flatten l =
     case l of
         [] -> []
         h::t -> h ++ flatten t
 
-type Player = {x:Int, y:Int}
+type Player = {x : Float, y : Float}
+
+blockCountPerRow = 10
+bcpr = blockCountPerRow
+hbcpr = bcpr / 2
+
+boardWidth = 800
+
+cellWidth = boardWidth / blockCountPerRow
+
+cw : Float
+cw = cellWidth
+hcw : Float
+hcw = cw / 2
+
 player : Player
 player = {x = 0, y = 0}
 
-movePlayer d player = {player | x <- player.x + d.x, y <- player.y + d.y}
-
-cellWidth = 50
-boardWidth = 900
-cw = cellWidth
+movePlayer d player = {player | x <- clamp -hbcpr (hbcpr - 1) player.x + toFloat d.x,
+                                y <- clamp -hbcpr (hbcpr - 1) player.y + toFloat d.y}
 
 square : Path
-square = path [ (cw,cw), (cw,-cw),
-                (-cw,-cw), (-cw,cw),
-                (cw,cw) ]
+square = path [ (hcw, hcw), (hcw,-hcw), (-hcw,-hcw), (-hcw,hcw), (hcw, hcw) ]
 
 blueSquare : Form
 blueSquare = traced (solid blue) square
 
-seq n = map ((*) cw) [0..n]
-board x y = map (\y' -> map (\x' -> move (x', y') blueSquare) (seq x)) (seq y)
+getPlayerSquare : Player -> Form
+getPlayerSquare p = move (p.x * cw + hcw, p.y * cw + hcw) (traced (solid red) square)
 
-main : Element
-main = collage boardWidth boardWidth (flatten (board 10 10))
+seq n = map ((*) cw) [-n / 2..(n - 1) / 2]
+getBoard x y = map (\y' -> map (\x' -> (x', y')) (seq x)) (seq y)
 
--- main = lift asText (foldp movePlayer player Keyboard.arrows)
+board = map (\p -> move p blueSquare)
+    (flatten (getBoard blockCountPerRow blockCountPerRow))
+
+playerState = foldp movePlayer player Keyboard.arrows
+
+display (w,h) p = container w h middle (collage boardWidth boardWidth (board ++ [getPlayerSquare p]))
+
+main : Signal Element
+main = lift2 display Window.dimensions playerState
