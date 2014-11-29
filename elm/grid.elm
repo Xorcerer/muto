c5fc9ee2bcaf1ddb47823aebd414262a849bd157
Logan Zhou <xorcererzc@gmail.com>
2014-11-29 23:38:02 +0800

    Added Firing bullets.

--- elm/grid.elm
+++ elm/grid.elm
@@ -8,9 +8,10 @@ flatten l =
         [] -> []
         h::t -> h ++ flatten t
 
-type Player = {x : Float, y : Float}
+type Vector = { x : Float, y : Float }
+type Player = { pos : Vector }
 
-type Bullet = {x: Float, y: Float, dx: Float, dy: Float}
+type Bullet = { x : Float, y : Float, dx: Float, dy: Float }
 
 
 blockCountPerRow = 10
@@ -28,40 +29,45 @@ hcw : Float
 hcw = cw / 2
 
 player : Player
-player = {x = 0, y = 0}
+player = { pos = { x = 0, y = 0} }
 
 bullets : [Bullet]
 bullets = []
 
 myClamp = clamp -hbcpr (hbcpr - 1)
 
-movePlayer d player = {player | x <- myClamp player.x + toFloat d.x,
-                                y <- myClamp player.y + toFloat d.y}
+outOfBoard x = myClamp x /= x
+movePlayer d player = {player | pos <- { x = myClamp player.pos.x + toFloat d.x,
+                                         y = myClamp player.pos.y + toFloat d.y}}
 
-fire : {x: Float, y:Float} -> {x: Float, y: Float} -> Bullet
-fire position direction = {x = position.x, y = position.y, dx = direction.x, dy = direction.y}
 
-updateBullets : [Bullet] -> [Bullet]
-updateBullets bullets = filterMap updateBullet bullets
+fire : Player -> {x: Float, y: Float} -> Bullet
+fire p direction = {x = p.pos.x, y = p.pos.y, dx = direction.x, dy = direction.y}
+
+updateBullets : BulletUpdate -> [Bullet] -> [Bullet]
+updateBullets event bullets =
+  case event of
+    FiredBy player -> fire player {x=0, y=1}::bullets
+    Time -> filterMap updateBullet bullets
 
 updateBullet : Bullet -> Maybe Bullet
 updateBullet b = let newBullet = {x = b.x + b.dx, y = b.y + b.dy, dx = b.dx, dy = b.dy}
-                in if (/=) (myClamp newBullet.x) newBullet.x || (/=) (myClamp newBullet.x) newBullet.x
+                in if outOfBoard newBullet.x || outOfBoard newBullet.y
                     then Nothing
                     else Just newBullet
 
 updateBullet_ : Player -> Bullet -> Bullet
-updateBullet_ player bullet = {bullet | x <- player.x, y <- player.y}
+updateBullet_ player bullet = {bullet | x <- player.pos.x, y <- player.pos.y}
 
 square : Path
-square = path [(hcw, hcw), (hcw,-hcw), (-hcw,-hcw), (-hcw,hcw), (hcw, hcw)]
+square = path [(hcw, hcw), (hcw, -hcw), (-hcw, -hcw), (-hcw, hcw), (hcw, hcw)]
 
 blueSquare : Form
 blueSquare = traced (solid blue) square
 
 showPlayer : Player -> Form
 showPlayer p =
-  move (p.x * cw + hcw, p.y * cw + hcw)
+  move (p.pos.x * cw + hcw, p.pos.y * cw + hcw)
   <| filled red
   <| circle hcw
 
@@ -72,7 +78,7 @@ showBullet : Bullet -> Form
 showBullet b =
   move (b.x * cw + hcw, b.y * cw + hcw)
   <| filled blue
-  <| circle hcw
+  <| circle (hcw / 2)
 
 seq n = map ((*) cw) [-n / 2..(n - 1) / 2]
 getBoard x y = map (\y' -> map (\x' -> (x', y')) (seq x)) (seq y)
@@ -81,16 +87,24 @@ board = map (\p -> move p blueSquare)
   <| flatten
   <| getBoard blockCountPerRow blockCountPerRow
 
-bullet = fire {x = 0, y = 0} {x = 0, y = 0}
 playerState = foldp movePlayer player Keyboard.arrows
 
-bulletsState : Signal Bullet
-bulletsState = sampleOn (Keyboard.isDown (Char.toCode ' ')) (foldp updateBullet_ bullet playerState)
+data BulletUpdate = Time | FiredBy Player
 
-display : (Int, Int) -> Player -> Bullet -> Element
+fireUnit = sampleOn (Keyboard.isDown <| Char.toCode ' ') playerState
+
+bulletUpdate : Signal BulletUpdate
+bulletUpdate = merge
+  (lift (always Time) (fps 2))
+  (lift FiredBy fireUnit)
+
+bulletsState : Signal [Bullet]
+bulletsState = foldp updateBullets bullets bulletUpdate
+
+display : (Int, Int) -> Player -> [Bullet] -> Element
 display (w,h) p bs = container w h middle
   <| collage boardWidth boardWidth
-  <| board ++ [showPlayer p] ++ (showBullets [bs])
+  <| board ++ [showPlayer p] ++ showBullets bs
 
 main : Signal Element
 main = lift3 display Window.dimensions playerState bulletsState
